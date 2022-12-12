953a71d8ee614a893fbd65eb8706c97e044cdc42
Archana Sharma <archanakumari202068@gmail.com>
2022-12-13 01:39:47 +0530

    Email_Sender code updated

---
+++ Email_Sender.py
@@ -0,0 +1,31 @@
+# go over to gmail account and setup 2 factor authentication
+# generate app password
+# create a function to send the mail
+#bayxxotpfbhckzrm     securesally@gmail.com
+
+
+from email.message import EmailMessage
+from app2 import password
+import ssl
+import smtplib
+
+
+
+email_sender = 'archanakumari202068@gmail.com'
+email_password = 'archu@202068' #security reason
+email_receiver = 'yilare5389@areosur.com'
+subject = "Testing"
+body = """
+Waiting for your reply...
+"""
+
+em = EmailMessage() #instance 
+em['from'] = email_sender
+em['To'] = email_receiver
+em['subjects'] = subject
+em.set_content(body)
+context = ssl.create_default_context ()
+with smtplib.SMTP_SSL('smtp.gmail.com',465,context=context) as smtp:
+    smtp.login(email_sender,email_password)
+    smtp.sendmail(email_sender,email_receiver,em.as_string())
+
