e8d99300277f29f92b0af9c1f97768100354ef6c
Archana Sharma <archanakumari202068@gmail.com>
2022-12-14 12:32:04 +0530

    Quiz_Program ques_ans added

---
+++ Quiz_Program.py
@@ -0,0 +1,42 @@
+# a dictionary that stores question and answers
+# have a variable that tracks the scores of the player
+# loop through the dictionary using the key values pairs
+# display each questions to the user and allow them to answer
+# tell them if they are right or wrong
+# show the final result when quiz is completed
+
+quiz = {
+    "question1": {
+        "question":"what is the capital of France?",
+        "answer":"Paris"
+
+    },
+    "question2":{
+        "question":"What is capital of Germany?",
+        "answer":"Berlin"
+    },
+        "question3":{
+        "question":"What is capital of Italy?",
+        "answer":"Rome"
+    }, 
+       "question4":{
+        "question":"What is capital of Spain?",
+        "answer":"Madrid"
+    }, 
+       "question5":{
+        "question":"What is capital of Portugal?",
+        "answer":"Lisbon"
+    }, 
+       "question6":{
+        "question":"What is capital of Switzerland?",
+        "answer":"Bern"
+    },
+        "question7":{
+        "question":"What is capital of Austria?",
+        "answer":"Vienna"
+    },
+
+
+}
+
+score = 0
