defa677df2713000a30914c6404fd8fb7813d2eb
Archana Sharma <archanakumari202068@gmail.com>
2022-12-14 16:11:37 +0530

    QR_Code_Generator code updated

---
+++ QR_Code_Generator.py
@@ -0,0 +1,23 @@
+# install all the libraries needed
+# pip3 install qrcode Image
+# created a function that collects a text and converts it to a qrcode
+# save the QRcode as an image 
+# run the function
+
+import qrcode
+
+def generate_qrcode(text):
+    qr = qrcode.QRCode(
+        version = 1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size= 10,
+        border=4,
+    )
+
+    qr.add_data(text)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color = "black",back_color="white")
+    img.save("qrimg.png")
+
+generate_qrcode("https://pay.google.com/")    
+
